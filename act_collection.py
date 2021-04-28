3420cae4f9b215870a87735fd719294882a0b738
DouBa <viphsj@gmail.com>
2021-04-28 14:36:05 +0800

    Create act_collection.py

---
+++ act_collection.py
@@ -0,0 +1,166 @@
+import jdCookie
+import json
+import requests
+import time
+import random
+import re
+import os,sys
+
+headers = {
+    'Host': 'lzkj-isv.isvjd.com',
+    'Accept': '*/*',
+    'Connection': 'close',
+    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+    'Accept-Language': 'zh-CN,zh;q=0.9',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Referer': 'https://lzkj-isv.isvjd.com/',
+    'Accept-Encoding': 'gzip, deflate, br',
+}
+
+headers_api = {
+    'Host': 'api.m.jd.com',
+    'Accept': '*/*',
+    'Connection': 'keep-alive',
+    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+    'Accept-Language': 'zh-CN,zh;q=0.9',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Referer': 'https://lzkj-isv.isvjd.com/',
+    'Accept-Encoding': 'gzip, deflate, br',
+}
+
+for cookies in jdCookie.get_cookies():
+    print("##"*30)
+    print("【开始获取活动信息】\n")
+    today = jdCookie.get_time_now().strftime("%Y%m%d")
+    errMsg = ""
+    url = os.environ["JD_SIGN_GETTOKEN_URL"]
+    params = {
+        }
+    data = os.environ["JD_SIGN_GETTOKEN_BODY"]
+    tokenInfo = requests.post(url, headers=headers_api, data=data, params=params, cookies=cookies)
+    token = tokenInfo.json()["token"]
+    time.sleep(1)
+
+    session = requests.session()
+    url = os.environ["JD_SIGN_URL"]
+    params = {        
+    }
+    session.get(url, headers=headers, params=params, cookies=cookies)
+    time.sleep(1)
+
+    data = {
+        'userId': 599119,
+        'token': token,
+        'fromType': 'APP'
+    }
+    secretPinInfo = session.post('https://lzkj-isv.isvjd.com/customer/getMyPing', headers=headers, data=data, params=params)
+    secretPin = secretPinInfo.json()["data"]["secretPin"]
+
+    url = 'https://lzkj-isv.isvjcloud.com/wxAssemblePage/getTopAndNewActInfo'
+    data = {
+        'pin': secretPin,
+        'aggrateActType': 8,
+        'topNewType': 2,
+        'pageNo': 1,
+        'pageSize': 100
+    }
+    getTopAndNewActInfo = session.post(url, headers=headers, data=data, params=params)
+    TopAndNewAct = getTopAndNewActInfo.json()["data"]["homeInfoResultVOList"]
+    
+    data = {
+        'pin': secretPin,
+        'aggrateActType': 8,
+        'topNewType': 1,
+        'pageNo': 1,
+        'pageSize': 100
+    }
+    getTopAndNewActInfo = session.post(url, headers=headers, data=data, params=params)
+    for newAct in getTopAndNewActInfo.json()["data"]["homeInfoResultVOList"]:
+        TopAndNewAct.append(newAct)
+
+    print("##"*30)
+    print("【关注商品有礼活动】")
+    for collectionActInfo in TopAndNewAct:
+        if collectionActInfo["activityTitle"] != "关注商品有礼":
+            continue
+        skipAdd = False
+        url = "https://lzkj-isv.isvjd.com/wxCollectionActivity/activity2/"+collectionActInfo["activityId"]+"?activityId="+collectionActInfo["activityId"]
+        response = session.get(url, headers=headers, cookies=cookies)        
+        if response.text.find("活动已经结束了") > -1:
+            errMsg = " 活动已结束1"
+            skipAdd = True
+        data = {
+            'pin': secretPin,
+            'activityId': collectionActInfo["activityId"]
+        }
+        response = session.post("https://lzkj-isv.isvjd.com/wxCollectionActivity/activityContent", headers=headers, data=data, params=params)
+        if response.text == '':
+            errMsg = " 活动已结束2"
+            skipAdd = True
+        elif response.json()["result"] == False:
+            errMsg = response.json()["errorMessage"]
+            skipAdd = True
+        if skipAdd == True:
+            print(f'{collectionActInfo["shopName"]} {errMsg}\n')
+            time.sleep(random.randint(2, 3))
+            continue
+        activityType = response.json()["data"]["type"]
+        cpvosList = response.json()["data"]["cpvos"]
+        hasCollectionSize = response.json()["data"]["hasCollectionSize"]
+        needCollectionSize = response.json()["data"]["needCollectionSize"]
+        needAddCartSize = needCollectionSize - hasCollectionSize
+        needFollow = response.json()["data"]["needFollow"]
+        hasFollow = response.json()["data"]["hasFollow"]
+        userId = response.json()["data"]["userId"]
+        print(collectionActInfo["shopName"])
+        if needAddCartSize > 0:
+            if needFollow == True and hasFollow == False:
+                data = {
+                    'userId': userId,
+                    'buyerNick': secretPin,
+                    'activityId': collectionActInfo["activityId"],
+                    'activityType': str(activityType)
+                }
+                followShop = session.post("https://lzkj-isv.isvjd.com/wxActionCommon/followShop", headers=headers, data=data, params=params)
+                if followShop.json()["result"] == True:
+                    print(f'关注店铺 成功')
+                else:
+                    print(f'关注店铺 失败  {followShop.json()["errorMessage"]}')
+                time.sleep(random.randint(2, 3))
+            for cpvosInfo in cpvosList:
+                hasAddCartSize = 0
+                if cpvosInfo["collection"] == False:
+                    data = {
+                        'productId': cpvosInfo["productId"],
+                        'pin': secretPin,
+                        'activityId': collectionActInfo["activityId"]
+                    }
+                    addCart = session.post("https://lzkj-isv.isvjd.com/wxCollectionActivity/addCart", headers=headers, data=data, params=params)
+                    if addCart.status_code == 200:
+                        if addCart.json()["result"] == False:
+                            continue
+                        hasAddCartSize = addCart.json()["data"]["hasAddCartSize"]
+                        time.sleep(random.randint(1, 3))
+                        print(f'关注商品数量 {str(addCart.json()["data"]["hasAddCartSize"])}/{str(needCollectionSize)}')
+                    else:
+                        continue
+                if hasAddCartSize == needCollectionSize:
+                    print("开始抽奖")
+                    break
+        else:
+            print(f'活动已完成 跳过\n')
+            time.sleep(random.randint(3, 6))
+            continue
+        time.sleep(random.randint(1, 3))
+        data = {
+            'pin': secretPin,
+            'activityId': collectionActInfo["activityId"]
+        }
+        getPrize = session.post("https://lzkj-isv.isvjd.com/wxCollectionActivity/getPrize", headers=headers, data=data, params=params)
+        if getPrize.json()["result"] == True:
+            print(f'获得：{getPrize.json()["data"]["name"]}\n')
+        else:
+            print(f'错误：{getPrize.json()["errorMessage"]}\n')        
+        time.sleep(random.randint(3, 6))
+    print("\n")
+    print("##"*30)
