dca3bfd3c690c78661784fa73e8bca5649ae932f
DouBa <viphsj@gmail.com>
2021-04-29 17:25:55 +0800

    Update act_addcart.py

--- act_addcart.py
+++ act_addcart.py
@@ -66,7 +66,7 @@ for cookies in jdCookie.get_cookies():
     }
     getTopAndNewActInfo = session.post(url, headers=headers, data=data, params=params)
     TopAndNewAct = getTopAndNewActInfo.json()["data"]["homeInfoResultVOList"]
-    
+    """
     data = {
         'pin': secretPin,
         'aggrateActType': 7,
@@ -77,7 +77,7 @@ for cookies in jdCookie.get_cookies():
     getTopAndNewActInfo = session.post(url, headers=headers, data=data, params=params)
     for newAct in getTopAndNewActInfo.json()["data"]["homeInfoResultVOList"]:
         TopAndNewAct.append(newAct)
-
+    """
     print("##"*30)
     print("【开始加购】\n")
     for addCartActInfo in TopAndNewAct:
